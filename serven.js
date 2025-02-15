60373511a72198e82f0b9bdd5dfec5b28c38992a
oarauju <daviaraujus33@gmail.com>
2025-02-15 18:29:44 -0300

    Update serven.js

--- serven.js
+++ serven.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 
-app.use(express.static('public')); // Servindo arquivos da pasta "public"
+app.use(express.static(__dirname));
 
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/index.html');
+    res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 const PORT = process.env.PORT || 3000;
